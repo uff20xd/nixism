2d2cabbf6e3a1d972ca5ace6cbeb0d5a46ea7c7e
uff20xd <donut121008@gmail.com>
2025-03-09 13:04:35 +0100

    k

--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,55 @@ use std::{
     },
     io::prelude::*,
 };
-
 use clap::{command, Parser};
+
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
 
-    #[arg(short, long)]
+    #[arg(short, long, default_value_t = ("None").to_string() )]
     install: String,
 
     #[arg(short, long, default_value_t = 1 )]
     remove: u8,
 
-    #[arg(long, default_value_t = true )]
-    init: bool,
+    #[arg(long="init", default_value_t = ("None").to_string() )]
+    init: String,
 
+    #[arg(long, default_value_t = ("None").to_string() )]
+    path: String,
+
+    #[arg(short = 'm', default_value_t = false)]
+    home_manager: bool,
+
+}
+
+fn create_package_file (path: String) -> std::io::Result<()> {
+    let mut file = File::create(path)?;
+    file.write_all(b"Hello World!")?;
+
+    Ok(())
 }
 
 fn main() {
     let args = Args::parse();
+    if args.init != *("None") {
+        let output = create_package_file(args.init.clone());
 
-    println!("Your installing the package: {}", args.install );
+        print!("{:?}", output);
+
+    } else {
+
+        if args.path != *("None"){
+
+        }
+        if args.install != *("None") {
+            println!("Your installing the package: {}", args.install );
+        };
+    }
+
+
+
     
 }
--- src/settings_manager.rs
+++ src/settings_manager.rs
@@ -1,1 +1,18 @@
-use serde;
+use serde::{Serialize, Deserialize};
+use std::default::Default;
+use confy;
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    path_to_nixos_config: String,
+    path_to_home_manager_config: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self { path_to_nixos_config: ("None").to_string(), path_to_home_manager_config: ("None").to_string()}
+    }
+    
+}
+
+
