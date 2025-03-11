6ddf3c932e6489e7bcc087028b05b1cf4d1387fc
uff20xd <donut121008@gmail.com>
2025-03-11 14:56:55 +0100

    k

--- src/settings_manager.rs
+++ src/settings_manager.rs
@@ -1,6 +1,6 @@
 use serde_derive::*;
 use std::{default::Default, io::Error, slice::ChunksMut};
-use confy;
+//use confy;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
@@ -8,11 +8,11 @@ pub struct Config {
     path_to_home_manager_config: String,
 }
 
-impl Default for Config {
-    fn default() -> Self {
-        Self { path_to_nixos_config: "None".to_string(), path_to_home_manager_config: "None".to_string() }
-    }
-}
+//impl Default for Config {
+//    fn default() -> Self {
+//        Self { path_to_nixos_config: "None".to_string(), path_to_home_manager_config: "None".to_string() }
+//    }
+//}
 
 pub fn change_nixos_path() -> Result<(), confy::ConfyError> {
     let cfg: () = confy::load("nixism", None)?;
