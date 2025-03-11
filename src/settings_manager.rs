6fe8ab23e8f2ea53dd2483b59726cce633d065b9
uff20xd <donut121008@gmail.com>
2025-03-11 21:52:15 +0100

    k

--- src/settings_manager.rs
+++ src/settings_manager.rs
@@ -1,12 +1,13 @@
 use serde_derive::*;
 use users::*;
-use toml::*;
 use std::{
     fs,
     fs::File,
     io::prelude::*,
     path::*,
 };
+
+use crate::settings_manager;
 
 
 //use confy;
@@ -32,15 +33,13 @@ pub struct Config {
 
 
 fn load_settings() -> Config {
-    let settings_file;
-    let current_user_as_osstring = get_current_username().expect("Couldnt get the username");
-    let current_user = current_user_as_osstring.to_str().expect("Just make it a string please");
-    let path = ("/home/").to_string() + current_user;
-    dbg!(&path);
+    let homedirectory= get_home_directory();
+    let settings_file: String;
+    dbg!(&homedirectory);
 
     // if the file exists it just reads from it
-    if Path::new(&(path.clone() + "/.config/nixism")).exists() {
-        settings_file = fs::read_to_string( path.clone() + "/.config/nixism/config.toml")
+    if Path::new(&(homedirectory.clone() + "/.config/nixism")).exists() {
+        settings_file = fs::read_to_string( homedirectory.clone() + "/.config/nixism/config.toml")
             .expect("Couldnt read the settings file");
 
         dbg!(&settings_file);
@@ -51,11 +50,11 @@ fn load_settings() -> Config {
     // If the File doesnt exist yet it will be created 
     else {
 
-        let _ = fs::create_dir(path.clone() + "/.config/nixism");
+        let _ = fs::create_dir(homedirectory.clone() + "/.config/nixism");
 
         let config = Config{path_to_nixos_config: ("None").to_string(), path_to_home_manager_config: ("None").to_string()};
 
-        let mut new_config_file = File::create(path.clone() + "/.config/nixism/config.toml")
+        let mut new_config_file = File::create(homedirectory.clone() + "/.config/nixism/config.toml")
             .expect("new config file in load setting");
 
         let new_config_file_contents = toml::to_string(&config)
@@ -64,7 +63,7 @@ fn load_settings() -> Config {
 
         let _ = write!(new_config_file, "{}", new_config_file_contents);
 
-        settings_file = fs::read_to_string(path.clone() + "/.config/nixism/config.toml").expect("cool");
+        settings_file = fs::read_to_string(homedirectory.clone() + "/.config/nixism/config.toml").expect("cool");
 
         toml::from_str(&settings_file)
             .expect("couldnt deserialize from file")
@@ -98,8 +97,7 @@ pub fn get_home_directory () -> String {
 pub fn handle_home_manager_settings (path_to_home_manager_config: String) {
     let mut config: Config = load_settings();
     dbg!(&config);
-    assert_eq!(config.path_to_home_manager_config, path_to_home_manager_config)
-
-
+    assert_eq!(config.path_to_home_manager_config, path_to_home_manager_config);
+    write_settings(config).expect("You seem to suck balls if you get this error");
 }
 
