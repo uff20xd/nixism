9687e6404ab965f16f0f4d4b0e5f527445c95fb9
uff20xd <donut121008@gmail.com>
2025-03-19 20:26:26 +0100

    k

--- flake.nix
+++ flake.nix
@@ -7,14 +7,23 @@
     let
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = nixpkgs.legacyPackages;
+      pkgsFor = forAllSystems (
+        system: 
+        import nixpkgs {
+          inherit system;
+          overlays = overlayList;
+        }
+      );
+
+      overlayList = [ self.overlays.default ];
     in {
+      overlays.default = final: prev: { nixism = final.callPackage ./default.nix { }; };
+
       packages = forAllSystems (system: {
         default = pkgsFor.${system}.callPackage ./default.nix { };
       });
-      nixosModules = forAllSystems (system:{
-        default = pkgsFor.${system}.callPackage ./default.nix { };
-      });
+
+      nixosModules = import ./default.nix { overlays = overlayList; };
       devShells = forAllSystems (system: {
         default = pkgsFor.${system}.callPackage ./shell.nix { };
       });
