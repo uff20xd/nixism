551e51d76367167dacbbc7eb65f9157fc3cbc919
uff20xd <donut121008@gmail.com>
2025-03-19 20:33:40 +0100

    k

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nix }:
     let
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
