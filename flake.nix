a897305d3ebaa1e1e8c212c5bf3b82b38fe2088e
uff20xd <donut121008@gmail.com>
2025-03-19 19:13:32 +0100

    k

--- flake.nix
+++ flake.nix
@@ -1,25 +1,20 @@
 {
-  description = "nixism - A tool to imperative be declaritive";
-
+  description = "Foo Bar";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgsFor = nixpkgs.legacyPackages;
-
     in {
-
       packages = forAllSystems (system: {
         default = pkgsFor.${system}.callPackage ./default.nix { };
       });
-
-      devShell = forAllSystems (system: {
+      devShells = forAllSystems (system: {
         default = pkgsFor.${system}.callPackage ./shell.nix { };
       });
-
-  };
+    };
 }
+
